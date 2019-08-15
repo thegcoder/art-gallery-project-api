9dbdc844f3d69495a0334326f9988266859482c8
Tai Glover <taigloverslaton@gmail.com>
2019-08-15 11:22:58 -0400

    completed index.js controllers

---
+++ controllers/index.js
@@ -0,0 +1,94 @@
+const { ArtWorks, Artists, Genre } = require('../models');
+
+// Get All
+async function all(type) {
+    let data = Promise.resolve(false);
+    switch (type) {
+        case 'ArtWorks':
+            data = await ArtWorks.find({});
+            return data;
+            break;
+        case 'users':
+            data = await Artists.find({});
+            return data;
+            break;
+        default:
+            return data;
+    }
+}
+
+// Create Operations
+async function create(type, object) {
+    let data = Promise.resolve(false);
+    console.log(`Type: ${type}`);
+    console.log(object);
+    switch (type) {
+        case 'ArtWorks':
+            data = await ArtWorks.create(object);
+            return data;
+            break;
+        case 'artists':
+            data = await Artists.create(object);
+            return data;
+            break;
+        default:
+            return data;
+    }
+}
+
+// Read Operations
+async function read(type, id) {
+    let data = Promise.resolve(false);
+    switch (type) {
+        case 'ArtWorks':
+            data = await ArtWorks.findById(id);
+            return data;
+            break;
+        case 'artists':
+            data = await artists.findById(id);
+            return data;
+            break;
+        default:
+            return data;
+    }
+}
+
+// Update Operations
+async function update(type, id, object) {
+    let data = Promise.resolve(false);
+    switch (type) {
+        case 'artworks':
+            data = await ArtWorks.findByIdAndUpdate(id, object, {new: true, upsert: true});
+            return data;
+            break;
+        case 'artists':
+            data = await Artists.findByIdAndUpdate(id, object, {new: true, upsert: true});
+            return data;
+            break;
+        default:
+            return data;
+    }
+}
+
+// Delete Operations
+async function deleter(type, id) {
+    let data = Promise.resolve(false);
+    switch (type) {
+        case 'artworks':
+            data = await ArtWorks.findByIdAndDelete(id);
+            return data;
+        case 'artists':
+            data = await Artists.findByIdAndDelete(id);
+            return data;
+        default:
+            return data;
+    }
+}
+
+module.exports = {
+   all,
+   create,
+   read,
+   update,
+   deleter
+}
