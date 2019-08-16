3ba6d6d4c936e0cc37823ad596c2b00943c56d64
Tai Glover <taigloverslaton@gmail.com>
2019-08-16 10:27:54 -0400

    updated models schemas

---
+++ models/index.js
@@ -0,0 +1,76 @@
+const mongoose = require('./connection.js');
+
+// Artwork schema
+const ArtWorkSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String
+    },
+    description: String,
+    imageUrl: {
+        type: String,
+        default: 'https://place-hold.it/300x500'
+    },
+    type: {
+        type: String,
+        default: 'artwork'
+    }
+});
+
+// Artists schema
+const ArtistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    bio: String,
+    imageUrl: {
+        type: String,
+        default: 'https://place-hold.it/300x500'
+    },
+    destinations: [],
+    type: {
+        type: String,
+        default: 'artists'
+    }
+});
+
+// Genre schema
+const GenreSchema = new mongoose.Schema({
+    country: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String
+    },
+    description: String,
+    imageUrl: {
+        type: String,
+        default: 'https://place-hold.it/300x500'
+    },
+    type: {
+        type: String,
+        default: 'genre'
+    }
+});
+
+// ArtWorks collection
+const ArtWorks = mongoose.model('ArtWorks', ArtworkSchema);
+
+// Users collection
+const Artists = mongoose.model('Artists', ArtistSchema);
+
+// Genres collection
+const Genres = mongoose.model('Genres', GenreSchema);
+
+
+module.exports = {
+  Artworks,
+  Artists,
+  Genres
+}
